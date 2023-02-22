2e1fd648443d1d79501351dac4762d87d611bb05
Wilmarie De La Cruz <wilmarie.delacruz@gmail.com>
2023-02-22 16:22:08 -0600

    updating exercices

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -0,0 +1,32 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String ShowChoices(){
+        return"/roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String ShowResults(@PathVariable int n, Model model){
+        int random = (int) Math.floor((Math.random() * 6) + 1);
+        System.out.println(random);
+        model.addAttribute("randomNum", random);
+
+        boolean result = random == n;
+
+        model.addAttribute("result", result);
+        return"/roll-dice";
+    }
+
+
+
+
+
+
+}
